0be746eda5365a9a5a737062f45f59371c814142
Waldo Panozo <waldo.panozo@gmail.com>
2022-08-08 15:24:48 -0400

    Starting with some samples

--- README.md
+++ README.md
@@ -8,5 +8,9 @@ We can start now!
 * Spreads
 * Destructure
 
+## Samples of some Exercises in JavaScript
+
+* Balancing brackets
+
 # Important:
 The order of the branches presented in this README is important, because are incremental for learning purpouses
---
+++ Samples/BalanceBrackets.js
@@ -0,0 +1,42 @@
+function balanceBrackets(str){
+    
+    let array = str.split(" ");
+
+    // console.log(array);
+
+    let abiertos = {
+        0 : "{",
+        1 : "[",
+        2 : "(",
+    };
+
+    let cerrados = {
+        0 : "}",
+        1 : "]",
+        2 : ")",
+    };
+    let arrayAbiertos = [];
+    array.forEach((element) => {
+        /**
+         * in the case of an open bracket
+         */
+        if(abiertos.includes(element)){
+            arrayAbiertos.push(element);   
+        }
+        /**
+         * in the case of a closed bracket
+         */
+        if(cerrados.includes(element)){
+            ///is must the inverse of the open bracket if not return false directly
+        }
+    });
+
+    console.log(arrayAbiertos);
+
+
+
+    
+
+}
+
+console.log(balanceBrackets("{ [ ] }"));
